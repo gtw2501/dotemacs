3de5ef0942d2fb83b6e28e0d8b7b423e7b406ada
Geoff Wark <gtw2501@protonmail.ch>
2017-10-31 21:46:30 -0400

    Add pianobar.el

--- init.el
+++ init.el
@@ -2,3 +2,7 @@
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+;; Pianobar.el stuff
+(add-to-list 'load-path "~/.emacs.d/elisp/pianobar.el/")
+(autoload 'pianobar "pianobar" nil t)
